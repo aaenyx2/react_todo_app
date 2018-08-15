6e2f44fa09ebe2f78d79f42b2340af7fc0906213
aaenyx2 <aaenyx2@mail.dgist.ac.kr>
2018-08-15 12:21:08 +0900

    immutable.js

--- src/App.js
+++ src/App.js
@@ -2,37 +2,57 @@ import React, { Component } from 'react';
 import Form from './components/Form';
 import TodoListTemplate from './components/TodoListTemplate';
 import TodoItemList from './components/TodoItemList';
+import { Map, List } from 'immutable';
 
 class App extends Component {
   id = 3
   state = {
-    input: '',
-    todos: [
-      { id: 0, text: ' 리액트 소개', checked: false },
-      { id: 1, text: ' 리액트 소개', checked: true },
-      { id: 2, text: ' 리액트 소개', checked: false }
-    ]
+    data: Map({
+      input : '',
+      todos : List([
+        Map({
+          id: 0, text: ' 리액트 소개', checked: false
+        }),
+        Map({
+          id: 1, text: ' 안녕', checked: true 
+          }),
+        Map({ 
+          id: 2, text: ' 리액트 소개', checked: false 
+        })
+      ])
+    })
   }
-
       
   handleChange = (e) => { // input box의 value 값이 바뀌는 event가 발생할 때마다
+    const { value } = e.target;
+    const { data } = this.state;
     this.setState({
-      input: e.target.value // input 의 다음 바뀔 값
+      data: data.set('input', value) // input 의 다음 바뀔 값
     });
   }
-    
+
   handleCreate = () => {
-    const { input, todos } = this.state;
+    const { data } = this.state;
     this.setState({
-      input: '', // 인풋 비우고
-      // concat 을 사용하여 배열에 추가
-      todos: todos.concat({
-        id: this.id++,
-        text: input,
-        checked: false
-      })
+      data: data.set('input', '').
+      update('todos', todos => todos.push(Map({
+        id: this.id++, text: data.get('input'), checked: false
+      })))
     });
   }
+
+  // handleCreate = () => {
+  //   const { data } = this.state;
+  //   this.setState({
+  //     input: '', // 인풋 비우고
+  //     // concat 을 사용하여 배열에 추가
+  //     todos: todos.concat({
+  //       id: this.id++,
+  //       text: input,
+  //       checked: false
+  //     })
+  //   });
+  // }
     
   handleKeyPress = (e) => {
     // 눌려진 키가 Enter 면 handleCreate 발생
@@ -41,38 +61,45 @@ class App extends Component {
     }
   }
 
-  handleToggle = (id) => {
-    // index에 해당하는 item의 버튼을 조작
-    const { todos } = this.state;
-    const index = todos.findIndex(todo => todo.id === id);
-    const selected = todos[index];
-    const nextTodos = [...todos]; // 배열을 복사
-    // Todo array의 해당 index 값을 덮어쓰기 한 뒤 이 array을 setState로 State에 반영
-    nextTodos[index] = { 
-      ...selected, 
-      checked: !selected.checked
-    };
 
+
+  handleToggle = (id) => {
+
+    // index에 해당하는 item의 버튼을 조작
+    const { data } = this.state;
+    const index = data.get('todos').findIndex(todo => todo.get('id') === id);
+
+    const updatedItem = data.get('todos').get(index).update('checked', checked => checked === 'true' ? 'false' : 'true');
     this.setState({
-      todos: nextTodos
+      data: {updatedItem}
     });
+
+
   }
 
   handleRemove = (id) => {
-    const { todos } = this.state;
-    this.setState({todos: todos.filter(todo => todo.id !== id)})
+    const { data } = this.state;
+    this.setState({
+      data: {
+        input: data.get('input'),
+        todos: data.get('todos').filter(todo => todo.id !== id)
+      }
+    });
   }
     
   render() {
-    const { input, todos } = this.state;
+    const { data } = this.state;
     const {
       handleChange,
       handleCreate,
       handleKeyPress,
       handleToggle,
-      onRemove,
       handleRemove
     } = this;
+
+    //FIX!!!!!!!!!!!
+    const input = data.get('input');
+    const todos = data.get('todos');
 
 
     return (
--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 import TodoItem from './TodoItem';
+import { Map, List } from 'immutable';
+
+
 
 class TodoItemList extends Component {
-    render() {
-        const {todos, onToggle, onRemove} = this.props;
+    
+    //todos.map 코드에서 Props 값을 읽어들일 때, 비동기적으로 데이터를 처리할 때,
+    // 데이터가 불러지기 전에 컴포넌트들은 무조건 한 번 렌더링된다. 이 때, todo가 undefined이기 때문에
+    // map of undefined 라는 오류가 발생한다. 이를 없애기 위해 todos 의 값을 초기화해줄 필요가 있다 
+    static defaultProps={
+        todos: List([])
+    };
 
+    render() {
+        
+    
+        const {todos, onToggle, onRemove} = this.props;
         const todoList = todos.map(
-            ({id, text, checked}) => (
+            (todo) => (
                 <TodoItem
-                    id={id}
-                    text={text}
-                    checked={checked}
+                    id={todo.get('id')}
+                    text={todo.get('text')}
+                    checked={todo.get('checked')}
                     onToggle={onToggle}
                     onRemove={onRemove}
-                    key={id}
+                    key={todo.get('id')}
                 />
             )
         );
